5cb94af8da1176c0802c267919929bc79a48312b
iiTzTimmy <573844716@qq.com>
2022-06-19 14:09:50 +0800

    update build

--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -18,27 +18,30 @@ export const zh = sidebar({
       prefix: "posts/",
       children: [
         {
-          text: "文章 1-4",
+          text: "导航",
           icon: "note",
           collapsable: true,
           prefix: "article/",
-          children: ["article1", "article2", "article3", "article4"],
+          children: ["常用的前端轮子", "网站", "工具整合"],
         },
         {
-          text: "文章 5-12",
+          text: "前端",
           icon: "note",
+          prefix: "front-end/",
           children: [
             {
-              text: "文章 5-8",
+              text: "JavaScript",
               icon: "note",
               collapsable: true,
-              prefix: "article/",
-              children: ["article5", "article6", "article7", "article8"],
+              prefix: "JavaScript/",
+              children: ["33个非常实用的JavaScript一行代码", "new命令原理", "ES5面向对象", "ES6面向对象", "多种数组去重性能对比", "比typeof运算符更准确的类型判断", "JS随机打乱数组", "判断是否为移动端浏览器", "将一维数组按指定长度转为二维数组", "防抖与节流函数", "JS获取和修改url参数"],
             },
             {
-              text: "文章 9-12",
+              text: "Vue",
               icon: "note",
-              children: ["article9", "article10", "article11", "article12"],
+              collapsable: true,
+              prefix: "Vue/",
+              children: ["基础/MVVM模式", "article10", "article11", "article12"],
             },
           ],
         },
